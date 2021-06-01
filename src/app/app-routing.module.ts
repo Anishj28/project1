a5e10b7b2fa4947d93172601294aa91362866bf7
manisha002 <63720721+manisha002@users.noreply.github.com>
2021-06-01 18:06:45 +0530

    added navbar, basic components and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ShowsComponent } from './shows/shows.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ {path:'home',component:HomeComponent},
+ {path:'movies', component:MoviesComponent},
+ {path:'shows', component: ShowsComponent},
+ {path:'watchlist', component: WatchlistComponent},
+ {path:'login', component: LoginComponent},
+ {path:'', redirectTo:"home", pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ShowsComponent } from './shows/shows.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    WatchlistComponent,
+    MoviesComponent,
+    ShowsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
